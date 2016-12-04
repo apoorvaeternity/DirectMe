697cb5cab4d06b4c9e922fba71f343ee4fd1b9bf
shobhitagarwal1612 <shobhitagarwal1612@gmail.com>
2016-12-04 14:56:35 +0530

    Registes Slot model in core.admin

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import ShipStore, Level, ShipUpgrade, Item, Island
+from .models import ShipStore, Level, ShipUpgrade, Item, Island, Slot
 
 admin.site.register(ShipStore)
 admin.site.register(Level)
 admin.site.register(ShipUpgrade)
 admin.site.register(Item)
 admin.site.register(Island)
+admin.site.register(Slot)
