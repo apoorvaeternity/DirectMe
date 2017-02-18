1b5fbeb9db4908b5659ded88f20a9a2cdac33324
Apoorva Pandey <apoorvapandey365@gmail.com>
2017-02-19 04:06:25 +0530

    Made changes in email settings.

--- direct_me/settings.py
+++ direct_me/settings.py
@@ -29,19 +29,6 @@ SECRET_KEY = '3__eu+zc19hdnok^+6a$dez25whb)r8z+_pvg%cmo#thna3nsh'
 DEBUG = True
 
 ALLOWED_HOSTS = ['direct-me.herokuapp.com', '127.0.0.1']
-EMAIL_HOST = ''
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-if 'IS_HEROKU' in os.environ:
-    EMAIL_HOST = os.environ.get('EMAIL_HOST')
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    EMAIL_PORT = os.environ.get('EMAIL_PORT')
-    EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
-
 
 # Application definition
 
@@ -104,6 +91,12 @@ DATABASES = {
         # 'PASSWORD': 'shobhit'
     }
 }
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 
 if 'IS_HEROKU' in os.environ:
     db_from_env = dj_database_url.config(conn_max_age=500)
