f805759d2f1faa54a306a8c856ff74b1e544645e
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2017-02-22 08:24:58 +0530

    Adds tests
    * Test case added for invalid token

--- player/urls.py
+++ player/urls.py
@@ -5,7 +5,7 @@ from .views import UserRegistrationView, UserAuthenticationView, UserView, GCMTo
     SuggestionListView, UsernameSearchView, EmailSearchView, EmailVerificationView, exchange_token
 
 urlpatterns = [
-    url(r'social/(?P<backend>[^/]+)/$', exchange_token),
+    url(r'social/(?P<backend>[^/]+)/$', exchange_token, name='google-login'),
     url(r'^email-verification/(?P<get_token>[^/]+)/$', EmailVerificationView.as_view(), name='email-verification'),
     url(r'^search-username/(?P<username>\w+)/$', UsernameSearchView.as_view(), name='search-username'),
     url(r'^search-email/(?P<email>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/$', EmailSearchView.as_view(),
